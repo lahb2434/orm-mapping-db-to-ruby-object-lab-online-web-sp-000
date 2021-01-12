23ffe66e41fa8399999bb2559ee06f1f6707b120
Chris Houghton <chrishoughton2434@gmail.com>
2021-01-12 15:13:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,6 +40,16 @@ class Student
     DB[:conn].execute(sql, num_X).map{|row| self.new_from_db(row)}
   end
   
+  def self.first_student_in_grade_10
+    sql = <<-POP 
+    SELECT * 
+    FROM students 
+    WHERE grade = '10'
+    POP
+    
+    DB[:conn].execute(sql, num_X).map{|row| self.new_from_db(row)}.first
+  end
+  
   def self.find_by_name(name)
     sql = "SELECT * FROM students WHERE name = ?"
     DB[:conn].execute(sql, name).map{|row| self.new_from_db(row)}.first
